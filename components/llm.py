e35d4fbd0e99539abbbaea47b5a796c5e6636fba
DimKouts84 <dimitriskouts@gmail.com>
2024-12-10 20:46:57 +0200

    Chat working. Before the conversation with more messages!!!
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   __init__ .py
    	modified:   components/llm.py
    	modified:   "pages/01 \360\237\244\226 AI_Analyst.py"
    	new file:   static/graph_schema.png

--- components/llm.py
+++ components/llm.py
@@ -1,82 +1,201 @@
-import streamlit as st
 from dotenv import load_dotenv
-import shelve
-from components import databases as db
-import datetime
- 
+from . import databases as db 
+# import databases as db 
+import os, requests, time
+
 load_dotenv()
 
-# Load chat history from shelve file
-def load_chat_history():
-    with shelve.open("chat_history") as db:
-        return db.get("messages", [])
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ LLM Models ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# OpenRouter
+or_llama33_70B_inst, llama_31_70_inst_free = 'meta-llama/llama-3.3-70b-instruct', 'meta-llama/llama-3.1-70b-instruct:free'
 
-# Save chat history to shelve file
-def save_chat_history(messages):
-    with shelve.open("chat_history") as db:
-        db["messages"] = messages
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ LLM Provider Connections ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+OPENROUTER_COMPLETIONS_URL = os.getenv("OPENROUTER_COMPLETIONS_URL")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL")
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ LLM Provider Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+provider_url_to_be_used = OPENROUTER_COMPLETIONS_URL
+provider_key_to_be_used = OPENROUTER_API_KEY
+provider_model_to_be_used = llama_31_70_inst_free
 
-# Initialize or load chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = load_chat_history()
-
-# Sidebar with a button to delete chat history
-with st.sidebar:
-    if st.button("Delete Chat History"):
-        st.session_state.messages = []
-        save_chat_history([])
-
-
-
-# A function that takes a question from the user to create a Cypher query
-def query_to_data_analyst(question):
-    graph_db_schma = """
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ Prompts ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+graph_db_schema = '''
     **Nodes**
-    [(:BENEFIT {name: "BENEFIT", indexes: [], constraints: []}), (:INDUSTRY {name: "INDUSTRY", indexes: [], constraints: []}), (:SKILL {name: "SKILL", indexes: [], constraints: []}), (:EXPERIENCE {name: "EXPERIENCE", indexes: [], constraints: []}), (:JOB {name: "JOB", indexes: ["embedding"], constraints: []}), (:RESPONSIBILITY {name: "RESPONSIBILITY", indexes: [], constraints: []})]
-    
-    **Relationships**
-    [[:NEEDS {name: "NEEDS"}], [:REQUIRES {name: "REQUIRES"}], [:HAS {name: "HAS"}], [:OFFERS {name: "OFFERS"}], [:POSTS {name: "POSTS"}]]
-    
-    **Node labels**
     ["INDUSTRY"] : ["industry_name", "standardized_industry_name"]
     ["JOB"] : ["employment_type", "employment_model", "job_seniority", "minimum_level_of_education", "standardized_occupation", "job_title", "country", "job_reference", "job_description"]
     ["SKILL"] : ["skill_name", "skill_category", "skill_type"]
     ["EXPERIENCE"] : ["minimum_years", "years_required"]
     ["BENEFIT"] : ["benefit_name"]
-    ["RESPONSIBILITY"] " ["description"]
-    """
+    ["RESPONSIBILITY"] : ["description"]
     
-    system_prompt = f'''
-    YYou are an expert Knowledge graph database analyst.
-    Your role is to help provide Cypher queries to the chatbot to retrieve information from the Neo4J Database.
-    Based on the schema of the graph database, here are the nodes, the node labels and relationships that you can query: {graph_db_schma}.
-    You will ONLY output a Cypher query, NO OTHER INFORMATION.'''
-    promt = f'''Based on the user's question provide a Cypher query to retrieve the information from the Neo4J Database. {question}?'''
+    **Relationships**
+    (INDUSTRY)-[POSTS]→(JOB)
+    (JOB)-[HAS]->(RESPONSIBILITY)
+    (JOB)-[REQUIRES]->(EXPERIENCE)
+    (JOB)-(OFFERS)->(BENEFIT)
+    (JOB)-[NEEDS]->(SKILL)
+    
+    '''
+
+cypher_query_examples = '''
+    - **Example 1:** 
+    -- Question: 'I want to know the skills required for an "accountant".'
+    -- Cypher: 
+        'MATCH (j:JOB)-[r]-(s:SKILL)
+        WHERE toLower(j.job_title) CONTAINS toLower("Accountant") OR toLower(j.standardized_occupation) CONTAINS toLower("Accountant")
+        RETURN s.skill_name AS skill_name, count(*) AS skill_count
+        ORDER BY skill_count DESC
+        LIMIT 100'
+    
+    - **Example 2:**
+    -- Question: 'I want to know the industries related to "food" what kind of jobs they offer / post.'
+    -- Cypher:
+        'MATCH (i:INDUSTRY)-[r]->(j:JOB)
+        WHERE toLower(i.industry_name) CONTAINS toLower("food")
+        WITH j.job_title AS job_title, count(*) AS job_count
+        RETURN job_title, job_count
+        ORDER BY job_count DESC
+        LIMIT 100'
+    
+    - **Example 3:**
+    -- Question: I have great knowledge of Excel and have a degree on Biology, what kind of jobs can I find?
+    -- Cypher:
+        '
+        '
+        
+    - **Example 4:**
+    -- Question: 'I'm a front-end developer and I know Django and Angular, what other skills are required for jobs?'
+    -- Cypher:
+        'MATCH (j:JOB)-[r]-(s:SKILL)
+        WHERE toLower(j.job_title) CONTAINS toLower("Front-end") OR toLower(j.job_title) CONTAINS toLower("Frontend")
+        RETURN s.skill_name AS skill_name, count(*) AS skill_count, j.job_title
+        ORDER BY skill_count DESC
+        LIMIT 100'
+        
+    - **Example 5:**
+    -- Question: 'I want to work from home, what kind of jobs offer this flexibility?'
+    -- Cypher:
+        'MATCH (j:JOB)
+        WHERE toLower(j.employment_model) CONTAINS toLower("remote") OR toLower(j.employment_model) CONTAINS toLower("hybrid") OR toLower(j.employment_type) CONTAINS toLower("remote") OR toLower(j.employment_type) CONTAINS toLower("home")
+        RETURN j.job_title AS job_title, j.standardized_occupation AS standardized_occupation, count(*) AS job_count, j.employment_model AS employment_model
+        ORDER BY job_count DESC
+        LIMIT 100'
+    '''
+
+
+data_engineer_system_prompt = f'''
+    You are an expert Knowledge graph database analyst.
+    Your role is to help provide Cypher queries to the chatbot to retrieve information from the Neo4J Database, following the instructions below:
+    1. Generate Cypher query compatible ONLY for Neo4j Version 5.
+    2. Do not use EXISTS, SIZE, HAVING keywords in the cypher. Use alias when using the WITH keyword.
+    3. Use only Nodes, Node labels, and Relationships that are mentioned in the given schema.
+    4. Always do a case-insensitive and fuzzy search for any properties related search.
+    6. You try to use similar keywords to the user's question to retrieve the information (e.g. `software developer`, `software engineer` and `software programmer`).
+    7. Never use relationships that are not mentioned in the given Graph Database Schema: {graph_db_schema}.
+    8. Here are some examples: {cypher_query_examples}
+    9. If the question does not need a Cypher query, please output `NONE` as a string.
+    
+    '''
+
+
+occupational_analyst_system_prompt = f'''
+    You are very excited and happy occupational expert analyst eager to answer questions about the job market in Cyprus.
+    Your role is to take data provided by a data engineer (if requested by the user) and answer to the user's questions.
+    Provide your using tables or bullet points to make your point more clear, if necessary.
+    Your output is always VERY professional and informative.
+    '''
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ Call LLM Function ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def call_LLM_API_JSON(provider_model_to_be_used, system_prompt, user_prompt_for_parsing, temperature=0):
+    # Note for Models that worked well, especially with the JSON mode:
+    ## Models with the best quality of output:  lmstudio-community/Qwen2.5-14B-Instruct-Q4_K_M.gguf,  lmstudio-community/Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf
+    ## Models that worked ok: MaziyarPanahi/Qwen2.5-7B-Instruct-Uncensored.Q5_K_S.gguf, bartowski/Llama-3.2-3B-Instruct-f16.gguf
+    
+    # Prepare the headers 
+    url = provider_url_to_be_used
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {provider_key_to_be_used}',
+    }
+
+    # Prepare the messages for the chat completion
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt_for_parsing}
+    ]
+    # Prepare the payload
+    payload = {
+        'model': provider_model_to_be_used,
+        'messages': messages,
+        "type": "json_object",
+        'temperature': temperature,
+        'max_tokens': 9216
+    }
+    
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+        # print(f"Response text: \n{response.json()}\n\n")
+        response.raise_for_status()  # Raise an error for bad status codes
+        # return response.json()['choices'][0]['message']['content']
+        succesful_response = response.json()['choices'][0]['message']['content']
+        full_response = response.json()
+        
+        if succesful_response:
+            return succesful_response
+        else:
+            return full_response
+
+    except requests.RequestException as e:
+        print(f"Error: {e} response from LLM API.")
+        time.sleep(60)  # Optional: wait a bit before retrying
+        return f"Error: {e} response from LLM API."
 
     
-    # answer = f"Here are some nodes related to your question: {data}"
-    return
-
-
-
-def query_to_occupational_analyst(question, data_from_chatbot):
     
-    system_prompt = f'''
-    You are very excited Occupational expert analyst eager to answer questions about the job market.
-    Your role is to take data provided by a data engineer and answer to the User's questions.'''
-    promt = f'''You will receive a question from a user
-    {question}?'''
-    with db.driver.session() as session:
-        query = f""" Answer to the user's question based on the data provided by the data engineer.
-        The data: {data_from_chatbot} \n
-        The user's question: {question}
-        Now be helpful and provide your answer.
-        """
-        result = session.run(query)
-        data = [record.data() for record in result]
-    # Format the data into a response
-    answer = f'''Here are some nodes related to your question: {data}'''
-    return answer
+# A function that takes a question from the user to create a Cypher query
+def query_to_data_engineer(data_engineer_system_prompt, question, temperature):
+    
+    user_promt = f'''
+    Based on the user's question provide a Cypher query to retrieve the information from the Neo4J Database. 
+    The question:{question}.
+    
+    Output if it is required by the question. The Cypher MUST as a string (e.g.: MATCH (j:JOB)-[r]-(s:SKILL)) NOT as Markdown (e.g.: using ```cypher ... ```) 
+    Output `NONE` if the question does not need a Cypher query.
+    '''
+    
+    response = call_LLM_API_JSON(provider_model_to_be_used, data_engineer_system_prompt, user_promt,temperature)
+    return response
+
+
+def query_to_occupational_analyst(occupational_analyst_system_prompt, question, graph_data, temperature):
+
+    user_promt = f'''
+    You will receive a question from a user
+    Question {question} \n
+    The data: {graph_data} \n
+    '''
+    
+    response = call_LLM_API_JSON(provider_model_to_be_used, occupational_analyst_system_prompt, user_promt, temperature)
+    return response
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~ Test the LLM Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# question = "Can you provide me with career advise? What should I have or do to be a Data Analyst?"
+# cipher_query = query_to_data_engineer(data_engineer_system_prompt, question, 0)
+# print(f"---- Cipher Query:\n{cipher_query}\n")
+
+# if cipher_query == "NONE":
+#     print("---- No Cypher query needed for the question.\n")
+#     graph_data = "No job related information available"
+# else:
+#     graph_data = db.get_job_data_from_Cypher(cipher_query)
+#     print(f"---- Graph Data:\n{graph_data}\n")
+
+# time.sleep(10)  # Optional: wait a bit before retrying
+
+# occupational_analyst_answer = query_to_occupational_analyst(occupational_analyst_system_prompt, question, graph_data, 1)
+# print(f"---- Occupational Analyst's Answer:\n{occupational_analyst_answer}\n")
 
--- pages/01 🤖 AI_Analyst.py
+++ pages/01 🤖 AI_Analyst.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from components import sidebar_auth
 from components import databases as db
-from components import llm as llm 
+from components import llm as llm
 
 # Page Configuration
 st.set_page_config(
@@ -17,7 +17,7 @@ CHATBOT_AVATAR = "😎"
 # Initialize auth in sidebar
 user_email = sidebar_auth.init_sidebar_auth()
 
-st.title("You personal AI Occupational Analyst and Career Coach")
+st.title("You AI Analyst and Career Coach")
 st.write("An advanced LLM chatbot with a comprehensive understanding of the job market, designed to help individuals navigate their career paths and keep professionals updated on the latest market trends.")
 
 
@@ -34,7 +34,8 @@ if not user_email:
     st.caption("Made with ❤️ by [Dimitris Koutsomichalis](https://github.com/DimKouts84/)")
     
     st.stop()
-    
+
+
 # If user is authenticated
 # Show the chatbot interface
 elif user_email:
@@ -44,9 +45,22 @@ elif user_email:
     user_question = st.text_input("Ask a question:")
     if st.button("Submit"):
         if user_question:
-            answer = db.query_to_data_analyst(user_question)
+            print(f"----User Question: \n{user_question}\n")
+            # Save the query to the database (use timestamp)
+            cipher_query = llm.query_to_data_engineer(llm.data_engineer_system_prompt, user_question, 0)
+            if cipher_query == "NONE":
+                print("---- No Cypher query needed for the question.\n")
+                graph_data = "No job related information available"
+            else:
+                print(f"---- Cypher query:\n{cipher_query}.\n")
+                graph_data = db.get_job_data_from_Cypher(cipher_query)
+                # Save Query in database (use timestamp)
+                print(f"---- Graph Data:\n{graph_data}\n")
+            
+            occupational_analyst_answer = llm.query_to_occupational_analyst(llm.occupational_analyst_system_prompt, user_question, graph_data, 0)
+    
             st.write("🚀 Answer:")
-            st.write(answer)
+            st.write(occupational_analyst_answer)
         else:
             st.warning("Please enter a question.")
     
